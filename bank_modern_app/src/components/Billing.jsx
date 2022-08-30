2e3fbe8d182fb49f5cd50172fc1f6c098916756e
VJS <minsung_96@naver.com>
2022-08-30 23:17:20 +0900

    Testimonial

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -17,7 +17,15 @@ const Billing = () => {
                 </h2>
                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
                     Lorem Ipsum
-                </p>   
+                </p>
+                <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+                    <img src={apple} alt="app_store"
+                    className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+                    />
+                    <img src={google} alt="google_play"
+                    className="w-[128px] h-[42px] object-contain cursor-pointer"
+                    />
+                </div>  
             </div>
         </section>
     )
