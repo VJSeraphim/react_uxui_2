6a678234c044b81f474339d88d6dc3bea2d5994f
VJS <minsung_96@naver.com>
2022-09-01 23:59:32 +0900

    FooterLinks

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import styles from '../styles'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+            <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+                <div className="flex-1 flex flex-col justify-start mr-10">
+                    <img 
+                        src={logo}
+                        alt="hoobank"
+                        className="w-[266px] h-[72px] object-contain"
+                    />
+                    <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+                        Lorem Ipsum
+                    </p>
+                </div>
+                <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+                    {footerLinks.map((link) => (
+                        <div key={footerLinks.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     )
 }
 
