600e2cfaae6904dcc99097e107f591525193a104
VJS <minsung_96@naver.com>
2022-09-06 00:01:35 +0900

    Footer

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -18,10 +18,41 @@ const Footer = () => {
                     </p>
                 </div>
                 <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-                    {footerLinks.map((link) => (
+                    {footerLinks.map((footerLink) => (
                         <div key={footerLinks.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-
+                            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+                                {footerLink.title}
+                                <ul className="list-none mt-4">
+                                    {footerLinks.footerLink.map((link, i) => (
+                                        <li key={link.name}
+                                            className={`font-poppins font-normal text-[16px] leading-[24px] 
+                                            text-dimWhite hover:text-secondary cursor-pointer
+                                            ${i !== footerLink.links.length -1 ? 'mb-4' : 'mb-0'}
+                                            `}
+                                        >
+                                            {link.name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </h4>
                         </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <p className="font-poppins font-medium text-[18px] text-center
+                text-[18px] leading-[27px] text-white" >
+                    2022 Lorem Ipsum All Rights Reserved.
+                </p>
+                <div className="flex flex-row md:mt-0 mt-6">
+                    {socialMedia.map((social, i) => (
+                        <img 
+                            key={social.id}
+                            src={social.icon}
+                            alt={social.id}
+                            className={`w-[21px] h-[21px] object-contain cursor-pointer
+                            ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+                        />
                     ))}
                 </div>
             </div>
