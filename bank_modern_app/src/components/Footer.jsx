55ea1d27748ab2a21c4d25f9f6dd1583841963be
VJS <minsung_96@naver.com>
2022-09-06 23:45:34 +0900

    Footer border

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
                     ))}
                 </div>
             </div>
-            <div>
+            <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]">
                 <p className="font-poppins font-medium text-[18px] text-center
                 text-[18px] leading-[27px] text-white" >
                     2022 Lorem Ipsum All Rights Reserved.
